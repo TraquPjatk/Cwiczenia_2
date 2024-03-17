9d72a8025489a6847a64873227bd79eddcaed651
TraquPjatk <s19787@pjwstk.edu.pl>
2024-03-17 22:46:08 +0100

    Average function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿//Console.WriteLine("Modyfikacja 2");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 5, 10, 15, 20, 25 };
+        Console.WriteLine(CalculateAverage(numbers));
+    }
+
+    static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        int sum = 0;
+        foreach (int value in array)
+        {
+            sum += value;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
